b55d4a612b166306eaef21247465795db827fb2d
Tamas Magyar <tamas.magyar@gmail.com>
2023-01-05 15:16:26 -0500

    Debug kickoff

--- axem/DEBUG/debug.sh
+++ axem/DEBUG/debug.sh
@@ -2,12 +2,12 @@
 
 echo "Deploy ..."
 
-docker_image_reg_name_deploy=$(jq -e '."deploy"' INFRASTRUCTURE/descriptor.json | tr -d \")
+docker_image_reg_name_deploy=$(jq -e '."deploy"' axem/descriptor.json | tr -d \")
 
 if [[ "x$(docker image ls --filter reference=${docker_image_reg_name_deploy} -q)" == "x" ]]; then  
   echo "Not find it create deploy docker image"  
   echo "Deploy: ${docker_image_reg_name_deploy}" 
-  pushd INFRASTRUCTURE/DEPLOY
+  pushd axem/DEPLOY
 	docker build -t ${docker_image_reg_name_deploy} .	
   popd
   
@@ -17,12 +17,12 @@ else
   echo "image ID:$(docker image ls --filter reference=${docker_image_reg_name_deploy} -q)" 
 fi
 
-docker_image_reg_name_build=$(jq -e '."build"' INFRASTRUCTURE/descriptor.json | tr -d \")
+docker_image_reg_name_build=$(jq -e '."build"' axem/descriptor.json | tr -d \")
 
 if [[ "x$(docker image ls --filter reference=${docker_image_reg_name_build} -q)" == "x" ]]; then  
   echo "Not find it create deploy docker image"  
   echo "Deploy: ${docker_image_reg_name_build}" 
-  pushd INFRASTRUCTURE/DEPLOY
+  pushd axem/BUILD
 	docker build -t ${docker_image_reg_name_build} .	
   popd
   
@@ -38,7 +38,7 @@ docker network rm my-net
 docker network create -d bridge my-net
 
 docker run -ti -d -v "$(pwd)":/work --privileged --network=my-net ${docker_image_reg_name_deploy} /bin/sh -c "st-util"
-docker run -ti -d -v "$(pwd)":/work --network=my-net ${docker_image_reg_name_build}
+docker run -ti -d -v "$(pwd)":/work --network=my-net ${docker_image_reg_name_build} /bin/sh -c "make; arm-none-eabi-gdb build/boardtest.elf"
 
 
 
