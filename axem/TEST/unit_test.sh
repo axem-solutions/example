b7347eb3d7918b5d14a9f452a56d5446ce04f80a
Tamas Magyar <tamas.magyar@gmail.com>
2023-02-02 15:10:44 -0500

    Add cpputest automatic generation

--- axem/TEST/unit_test.sh
+++ axem/TEST/unit_test.sh
@@ -2,6 +2,21 @@
 
 echo "Running unit tests ..."
 
+docker_image_reg_name=$(jq -e '."cpputest"' axem/descriptor.json | tr -d \")
+
+if [[ "x$(docker image ls --filter reference=${docker_image_reg_name} -q)" == "x" ]]; then  
+  echo "Not find it create cpputest docker image"  
+  echo "Cpputest: ${docker_image_reg_name}" 
+  pushd axem/TEST
+	docker build -t ${docker_image_reg_name} .	
+  popd
+  
+else
+  echo "${docker_image_reg_name}" 
+  echo "Docker image exist"
+  echo "image ID:$(docker image ls --filter reference=${docker_image_reg_name} -q)" 
+fi
+
 echo $(pwd)
 docker run --rm -v "$(pwd)":/work --name cpputest_container cpputest /bin/sh -c "cd app/test; make"
 
