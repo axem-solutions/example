0bb0ba451b3901f11a2385fe57e93ea6f69d6ef4
janosmurai <murai.janos@gmail.com>
2023-07-05 14:34:13 +0200

    The descriptor only contains the name of the used Dev Env. The scripts updated to use the DEM's run command.

--- .axem/BUILD/build.sh
+++ .axem/BUILD/build.sh
@@ -1,29 +1,11 @@
 #!/bin/bash
 
-echo "Build ..."
-
-docker_image_reg_name=$(jq -e '."build"' .axem/descriptor.json | tr -d \")
-
-if [[ "x$(docker image ls --filter reference=${docker_image_reg_name} -q)" == "x" ]]; then  
-  echo "Not find it! \n Download: ${docker_image_reg_name}" 
-	docker pull ${docker_image_reg_name}
-  
-else
-  echo "${docker_image_reg_name}" 
-  echo "Docker image exist"
-  echo "image ID:$(docker image ls --filter reference=${docker_image_reg_name} -q)" 
-fi
-
+dev_env=$(jq -e '."dev_env"' .axem/descriptor.json | tr -d \")
 
 if [[ "${PROJECT_DIR}" == "" ]]; then  
-  docker run --rm -v "$(pwd)":/work ${docker_image_reg_name} /bin/sh -c "ls -la; make"
-  echo "here"
+  echo "Dev build ..."
+  dem run "${dev_env}" "build system" "$(pwd)" "make"
 else
-  docker run --rm -v "${PROJECT_DIR}":/work ${docker_image_reg_name} /bin/sh -c "ls -la; make"
+  echo "CI build ..."
+  dem run "${dev_env}" "build system" "${PROJECT_DIR}" "make"
 fi
-
-
-
-
-
-
--- .axem/DEPLOY/deploy.sh
+++ .axem/DEPLOY/deploy.sh
@@ -1,29 +1,6 @@
 #!/bin/bash
 
+dev_env=$(jq -e '."dev_env"' .axem/descriptor.json | tr -d \")
+
 echo "Deploy ..."
-
-docker_image_reg_name=$(jq -e '."deploy"' .axem/descriptor.json | tr -d \")
-
-if [[ "x$(docker image ls --filter reference=${docker_image_reg_name} -q)" == "x" ]]; then  
-  echo "Not find it create deploy docker image"  
-  echo "Deploy: ${docker_image_reg_name}" 
-  pushd .axem/DEPLOY
-	docker build -t ${docker_image_reg_name} .	
-  popd
-  
-else
-  echo "${docker_image_reg_name}" 
-  echo "Docker image exist"
-  echo "image ID:$(docker image ls --filter reference=${docker_image_reg_name} -q)" 
-fi
-
-output=$(docker ps --all --filter name=stlink_org_container -q)
-if [ ! -z "$output" ]
-then 
-  docker rm stlink_org_container
-fi
-
-echo $(pwd)
-docker run -it --privileged -v "$(pwd)":/work --name stlink_org_container ${docker_image_reg_name} /bin/bash -c "ls -la; cd build; st-flash write boardtest.bin 0x8000000"
-
-
+dem run --privileged "${dev_env}" "deployer" "$(pwd)" "/bin/sh -c \"cd build; st-flash write boardtest.bin 0x8000000\""
